4003a735060b5065d842ff0151899ef92b663bc8
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-09 16:57:07 +0330

    change commit end project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import Modal from './modal/Modal';
 function App() {
   return (
     <div className='container'>
-     <Modal>
-     <p>پرداخت شما با موفقیت انجام شد😀</p>
-            <ul className='cd-buttons'>
-                <li><a href="#">تایید</a></li>
-                <li><a href="#">بازگشت</a></li>
-            </ul>
-      </Modal>
+     <Modal/>
+    
     </div>
   );
 }
--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import './Modal.css'
-export default function Modal({children}) {
+export default class Modal extends React.Component {
   
+  render(){
     return (
       <div>
         <div className='cd-popup is-visible' role='alert'>
            <div className='cd-popup-container'>
-            {children}
+           <p>{this.props.message}</p>
+            <ul className='cd-buttons'>
+                <li><a href="#">تایید</a></li>
+                <li><a href="#">بازگشت</a></li>
+            </ul>
             <a  href="#0" className='cd-popup-close img-replace'>X</a>
            </div>
         </div>
       </div>
     )
   }
+ 
+  }
 
+Modal.defaultProps= {
+message:'شما با موفقیت وارد پنل کاربریتان شدید'
+}
